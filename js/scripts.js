51f0eac1ae14f155c7dff7715d6ccadea3846078
hectorob91 <hectorob91@gmail.com>
2018-03-19 21:44:56 -0600

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function iniciarGaleria(numeroImagenes){
+	var url = "./img/";
+	var header = document.getElementById('header');
+	var nombres = [
+		"nombre uno",
+		"nombre dos",
+		"nombre tres",
+		"nombre cuatro",
+		"nombre cinco",
+		"nombre seis",
+		"nombre siete",
+		"nombre ocho",
+		"nombre nueve",
+		"nombre diez",
+		"nombre once",
+		"nombre doce"
+	]; 
+	for (var i = 0; i < numeroImagenes; i++) {
+		var numImg = i+1;
+        var create_img = document.createElement("img");
+        create_img.src = url + numImg + ".jpg";
+        create_img.alt = nombres[i];
+        create_img.classList.add("imageStyle");
+        header.appendChild(create_img);
+    }
+}
